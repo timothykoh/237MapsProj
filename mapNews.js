ae6d416847c83812cdfd604c3d610f940354f0b6
Timothy Koh <k.o.htimothy@gmail.com>
2012-09-29 18:41:38 -0400

    Added pop up boxes when you mouse over a marker
    Pop up boxes provide news details: headlines, content etc..

--- mapNews.js
+++ mapNews.js
@@ -1,4 +1,13 @@
-var map;
+function Map(){
+	this.mapDisplay;
+}
+
+Map.prototype.infoBox = new InfoBox({	
+	content: ""
+	,closeBoxURL: ""
+	,pixelOffset: new google.maps.Size(-170,-160)
+});
+var map = new Map();
 
 window.onload = function(){
 	var mapOptions = {
@@ -6,7 +15,7 @@ window.onload = function(){
 		zoom: 8,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
-	map = new google.maps.Map(document.getElementById("mainMap"), mapOptions);
+	map.mapDisplay = new google.maps.Map(document.getElementById("mainMap"), mapOptions);
 }
 
 
@@ -27,7 +36,7 @@ function addMarker(){
 		var img = new google.maps.MarkerImage(document.getElementById("imgURL").value, //url
 												new google.maps.Size(imgWidth,imgHeight), //size
 												new google.maps.Point(0,0), //origin - position of image;
-												new google.maps.Point(0,0), //anchor
+												new google.maps.Point(imgWidth/2,imgHeight/2), //anchor
 												new google.maps.Size(imgWidth, imgHeight)); //scaled size
 		//define clickable region
 		var shape = {
@@ -37,16 +46,36 @@ function addMarker(){
 
 		var marker = new google.maps.Marker({
 			position: latLng,
-			map: map,
+			map: map.mapDisplay,
 			icon: img,
 			shape: shape,
+			headline: document.getElementById("newsHeadline").value,
+			content: document.getElementById("newsContent").value,
 			url: document.getElementById("linkURL").value,
 			title: document.getElementById("locationName").value
 		});
 
-		google.maps.event.addListener(marker, 'click', function() {
-    		window.location.href = this.url;
+		google.maps.event.addListener(marker, 'mouseover', function() {
+    		map.infoBox.open(map.mapDisplay, marker);
+    		
+    		var headline = document.createElement("h1");
+    		headline.innerHTML = this.headline;
+    		var content = document.createElement("h2");
+    		content.innerHTML = this.content;
+
+    		var infoBoxContent = document.createElement("div");
+    		infoBoxContent.appendChild(headline);
+    		infoBoxContent.appendChild(content);
+
+    		map.infoBox.setContent(infoBoxContent);
+    		
 		});
+
+		google.maps.event.addListener(marker, 'click', function(){
+			window.location.href = this.url;
+		});
+
+
 	}
 		
 }
